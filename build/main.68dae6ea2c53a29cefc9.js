6087e02bcca4bc498ae8b73a4ff4263901d0e6fa
SonOfSteveJobs <evgenev2222@gmail.com>
2023-11-18 15:27:51 +0300

    add mini css plugin

--- build/main.68dae6ea2c53a29cefc9.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log("THE SUM IS:",10)})();
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 type Mode = 'production' | 'development';
@@ -11,15 +12,29 @@ interface EnvVariables {
 
 module.exports = (env: EnvVariables) => {
     const isDevMode = env.mode === 'development';
+    const isProdMode = env.mode === 'production';
 
     const config = {
         mode: env.mode ?? 'development',
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'public', 'index.html')
+            }),
+            isProdMode && new MiniCssExtractPlugin({
+                filename: 'css/[name].[contenthash:8].css',
+                chunkFilename: 'css/[name].[contenthash:8].css',
+            })
+        ].filter(Boolean),
         module: {
             rules: [
                 {
                     test: /\.s[ac]ss$/i,
-                    use: ["style-loader", "css-loader", "sass-loader"],
+                    use: [
+                        isDevMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader"
+                    ],
                 },
                 {
                     test: /\.tsx?$/,
@@ -36,11 +51,6 @@ module.exports = (env: EnvVariables) => {
             filename: '[name].[contenthash].js',
             clean: true
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, 'public', 'index.html')
-            }),
-        ].filter(Boolean),
         devServer: isDevMode ? {
             port: env.port ?? 3000,
             open: true
