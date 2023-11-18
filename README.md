072ce0344f39bea15f3e14ad4bfc722a753fa12c
SonOfSteveJobs <evgenev2222@gmail.com>
2023-11-18 15:00:07 +0300

    add css/scss

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Env variables support
 2. Typescript support (config is also typed)
 3. Webpack dev server
+4. Sass support
--- webpack.config.ts
+++ webpack.config.ts
@@ -18,6 +18,10 @@ module.exports = (env: EnvVariables) => {
         module: {
             rules: [
                 {
+                    test: /\.s[ac]ss$/i,
+                    use: ["style-loader", "css-loader", "sass-loader"],
+                },
+                {
                     test: /\.tsx?$/,
                     use: 'ts-loader',
                     exclude: /node_modules/,
